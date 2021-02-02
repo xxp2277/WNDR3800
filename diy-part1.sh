89011f1ad29790d7111d8bab76186819abb9f02e
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2021-02-02 22:52:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
+#
+./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i 's/+luci-theme-bootstrap / /g' ./feeds/luci/collections/luci/Makefile
